02c84082c27c2e2984aef819320f93d71801a662
Blessed Sode <emi.square23@gmail.com>
2025-08-26 13:20:27 +0100

    added router basenames

--- fitness/tracker/urls.py
+++ fitness/tracker/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import FitnessDataViewSet, ActivityTypeViewSet, ActivitySummaryViewSet
 
 router = DefaultRouter()
-router.register(r'fitness', FitnessDataViewSet)
-router.register(r'activity', ActivityTypeViewSet)
-router.register(r'summary', ActivitySummaryViewSet)
+router.register(r'fitness', FitnessDataViewSet, basename='fitness-data')
+router.register(r'activity', ActivityTypeViewSet, basename='activity-type')
+router.register(r'summary', ActivitySummaryViewSet, basename='activity-summary')
 
 urlpatterns = [
     path('', include(router.urls)),
