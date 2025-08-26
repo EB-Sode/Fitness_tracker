d402978ade87357038d1ca152c9e9ecc68fdc670
Blessed Sode <emi.square23@gmail.com>
2025-08-26 13:16:08 +0100

    added urls for the views

--- fitness/tracker/urls.py
+++ fitness/tracker/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import FitnessDataViewSet, ActivityTypeViewSet
+from .views import FitnessDataViewSet, ActivityTypeViewSet, ActivitySummaryViewSet
 
 router = DefaultRouter()
 router.register(r'fitness', FitnessDataViewSet)
 router.register(r'activity', ActivityTypeViewSet)
-
+router.register(r'summary', ActivitySummaryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls')),
 ]
